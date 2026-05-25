6902366c1d22077b4791b657efd3c6d11764a13b
RaulCarSan <racasan2002@gmail.com>
2026-05-25 17:01:26 +0200

    Primer commit

---
+++ src/main/java/org/iesalandalus/ChatDAM/ChatDamApplication.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.ChatDAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatDamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalandalus/ChatDAM/ChatDamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.ChatDAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
